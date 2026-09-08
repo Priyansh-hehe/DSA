4de1f3c1f750b98ccb2e73f36e0e8636ad65cbad
Priyansh Paliwal <priyansh8paliwal@gmail.com>
2026-09-08 23:11:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.38%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        
+        int comma = 1;
+        if(n<=999){
+            return 0;
+        }
+        int result = n - 999;
+        return result;
+        }
+};
